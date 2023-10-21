3254592f01ef1659c6a91a10293364db5f2715df
youness <younesserbai@gmail.com>
2023-10-21 17:36:34 +0200

    page contact first commit

---
+++ test.js
@@ -0,0 +1,6 @@
+function show() {
+  document.getElementsByClassName("siide")[0].style.display = "flex";
+}
+function hid() {
+  document.getElementsByClassName("siide")[0].style.display = "none";
+}
